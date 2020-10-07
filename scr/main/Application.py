89064c00c4779667468a837ebbe00204e81cae13
Gabriel Torrezan <36551976+Scandiuzzii@users.noreply.github.com>
2020-10-07 12:24:54 -0300

    Imagem inserida, porem build não executando

--- scr/main/Application.py
+++ scr/main/Application.py
@@ -1,14 +1,18 @@
 from tkinter import *
+
 from main import cadastrar_rfid,ler_rfid
 
-class Application(Tk):
-    def __init__(self):
-        super().__init__()
-        self.root = Tk()
+class Application():
+    def __init__(self, root):
+        self.tag = "123456789"
+        self.root = root
         self.tela()
         self.frame()
         self.title()
         self.botoes()
+        mainloop()
+
+
 
     def tela(self):
         self.root.title("Interface do RFID")
@@ -19,20 +23,41 @@ class Application(Tk):
     def frame(self):
         self.frame = Frame(self.root, bd = 4, bg = "#fff", highlightbackground="#759fe6", highlightthickness=2)
         self.frame.place(relx=0.1, rely= 0.1, relwidth= 0.8, relheight= 0.8)
+
+        imagem = PhotoImage(file="main/icons/teste.png")
+        self.w = Label(self.root, image=imagem, bg="#fff")
+        self.w.imagem = imagem
+        self.w.place(relx=0.785, rely= 0.71)
+        
+      
+
     
     def title(self):
-        self.title = Label(self.frame, text="Interface RFID")
+        self.title = Label(self.frame, text="Interface RFID", bg="#fff")
         self.title["font"] = ("Arial", "14", "italic", "bold")
         self.title.pack()
 
-    def botoes(self):
-        self.cadastrar = Button(self.frame, text="Cadastrar",command=cadastrar_rfid)
-        self.cadastrar.place(relx=0.15, rely=0.35, relwidth=0.3, relheight= 0.2)
+   
 
+
+    def botoes(self):
         self.leitura = Button(self.frame, text="Leitura")
-        self.leitura.place(relx=0.55, rely=0.35, relwidth=0.3, relheight= 0.2)
+        self.leitura.place(relx=0.15, rely=0.3, relwidth=0.2, relheight= 0.15)
+
+
+        self.lb_leitura = Label(self.frame, text="Código da Tag : ", bg="#fff")
+        self.lb_leitura.place(relx=0.38, rely=0.3, relwidth=0.22, relheight= 0.15)
+
+        self.entry_leitura = Label(self.frame, text=self.tag)
+        self.entry_leitura["font"] = ("Arial", "10", "bold")
+        self.entry_leitura.place(relx=0.65, rely=0.3, relwidth=0.2, relheight= 0.15)
+
+        self.cadastrar = Button(self.frame, text="Cadastrar",command=cadastrar_rfid, bg="green", fg="white")
+        self.cadastrar.place(relx=0.35, rely=0.6, relwidth=0.3, relheight= 0.2)
+    
+        
  
 
 if __name__ == '__main__':
-    app = Application()
-    app.mainloop()
+    root = Tk()
+    app = Application(root)
